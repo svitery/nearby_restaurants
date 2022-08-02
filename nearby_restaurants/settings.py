a1621b7da00f04b8b9c15386bb84ddabde635f16
svitery <svitery@alfred.com.co>
2022-08-02 00:10:47 -0500

    Removed secret

--- nearby_restaurants/settings.py
+++ nearby_restaurants/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_lh3$40%ux*lhyhxz4_rbzvg1w47ikr=pce%)%f)l9y$3=4%cp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
